39be1b26acb23f82dee5791b1ba7d2606232ca3a
Ouedraogo Filwendé Souley-Mady Serges <95701548+souleymady530@users.noreply.github.com>
2023-08-28 17:13:46 +0000

    Start

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './Components/Vitrine/header/header.component';
+import { FooterComponent } from './Components/Vitrine/footer/footer.component';
+import { BodyComponent } from './Components/Vitrine/body/body.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    BodyComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
